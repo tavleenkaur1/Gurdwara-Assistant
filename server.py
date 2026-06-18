b158c34061e2420f57087b70603f226299a91652
tavleenkaur1 <mailtotavleen@gmail.com>
2026-06-17 17:54:20 -0700

    fix client

--- server.py
+++ server.py
@@ -6,8 +6,7 @@ app = Flask(__name__, template_folder='.')
 
 HFkey = os.environ.get("API_KEY")
 
-client = InferenceClient(model="meta-llama/Meta-Llama-3-8B-Instruct", token=HFkey)
-
+client = InferenceClient(base_url="https://api-inference.huggingface.co/v1", model="meta-llama/Meta-Llama-3-8B-Instruct", token=HFkey)
 @app.route('/')
 def home():
     return render_template('index.html')
